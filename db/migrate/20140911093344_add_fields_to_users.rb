30463669e880689717c12434ac1e0021a7e41830
ahmadhasankhan <er.ahmad.hassan@gmail.com>
2014-09-13 11:38:12 +0545

    Cleanup

--- db/migrate/20140911093344_add_fields_to_users.rb
+++ db/migrate/20140911093344_add_fields_to_users.rb
@@ -1,7 +1,6 @@
 class AddFieldsToUsers < ActiveRecord::Migration
   def change
   	add_column :users, :username, :string
-    # add_column :users, :subdomain, :string
     add_column :users, :gender, :string
     add_column :users, :contact, :string
     add_column :users, :address_line1, :string
