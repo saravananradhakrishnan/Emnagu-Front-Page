84590829df0591fbcdde183d7db27daf7d71c0bb
Pulkit Agarwal <pulkit.ag02@gmail.com>
2014-09-13 09:43:17 +0530

    Override the the user information & implement the email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
-  before_filter :configure_devise_params, if: :devise_controller?
-  def configure_devise_params
-    devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:user_name, :subdomain, :gender, :contact,:email, :password, :password_confirmation)
+  protected
+
+  def configure_devise_permitted_parameters
+    registration_params = [:username, :email, :password, :password_confirmation]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(registration_params << [:current_password, :gender, :contact, :address_line1, :address_line2])
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) { 
+        |u| u.permit(registration_params) 
+      }
     end
   end
 end
--- db/migrate/20140911093344_add_fields_to_users.rb
+++ db/migrate/20140911093344_add_fields_to_users.rb
@@ -1,8 +1,10 @@
 class AddFieldsToUsers < ActiveRecord::Migration
   def change
-  	add_column :users, :user_name, :string
-    add_column :users, :subdomain, :string
+  	add_column :users, :username, :string
+    # add_column :users, :subdomain, :string
     add_column :users, :gender, :string
     add_column :users, :contact, :string
+    add_column :users, :address_line1, :string
+    add_column :users, :address_line2, :string
   end
 end
