11493388f7902953bb5b8134d88ce54847dd5069
ahmadhasankhan <er.ahmad.hassan@gmail.com>
2014-09-18 18:14:14 +0545

    updated MenuBar

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,10 +16,15 @@ if Rails.env.production?
     config.root = Rails.root.join('tmp') 
     config.cache_dir = 'carrierwave' 
     config.fog_credentials = {
-      :provider               => 'AWS',                     
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                   
-      :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],                        
+      # :provider               => 'AWS',                     
+      # :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                   
+      # :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],   
+
+      :provider               => 'AWS',                 
+      :aws_access_key_id      => 'AKIAIR7AP3IO3NAEKIBQ',                      
+      :aws_secret_access_key  => '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',                            
     }
-    config.fog_directory  = ENV['S3_BUCKET']
+    #config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_directory  = 'emangubucket' 
   end
 end
